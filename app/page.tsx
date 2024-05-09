6151c311570315e12d4b22ab1512506aa325bf96
daehyeong2 <baconbacon1231@gmail.com>
2024-05-09 16:18:50 +0900

    Group Modifiers (3.9)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full max-w-screen-md shadow-lg p-5 rounded-3xl flex flex-col md:flex-row justify-center gap-3">
+      <div className="bg-white w-full max-w-screen-md shadow-lg p-5 rounded-3xl flex flex-col justify-center gap-3 group has-[:hover]:bg-red-300">
         {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
           <div key={index} className="flex items-center gap-5">
             <div className="size-9 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium empty:w-24 empty:h-3 empty:bg-gray-300 empty:rounded-full empty:animate-pulse">
+            <span className="group-hover:text-red-500 text-lg font-medium empty:w-24 empty:h-3 empty:bg-gray-300 empty:rounded-full empty:animate-pulse">
               {person}
             </span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+            <div className="ml-auto size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
               <span className="z-10">{index + 1}</span>
               <div className="animate-ping size-6 bg-red-500 rounded-full absolute" />
             </div>
