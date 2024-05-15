46a6933625e0ee7ba3ecbc478e59373f504da53b
daehyeong2 <baconbacon1231@gmail.com>
2024-05-15 10:58:45 +0900

    Matcher (8.10)

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { NextRequest } from "next/server";
-import getSession from "./lib/session";
 
 export async function middleware(req: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (req.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", req.url));
-  }
+  console.log("d");
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
