f30c8a10def8488c90821bac4b79ad2260b00c30
daehyeong2 <baconbacon1231@gmail.com>
2024-05-18 16:23:52 +0900

    Product Detail (10.5)

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,14 +1,101 @@
-async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 60000));
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound, redirect } from "next/navigation";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+  return false;
 }
 
-const ProductDetail = async ({
-  params: { id },
-}: {
-  params: { id: string };
-}) => {
-  const product = await getProduct();
-  return <span>Product detail of the product {id}</span>;
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return product;
+}
+
+const ProductDetail = async ({ params }: { params: { id: string } }) => {
+  const id = Number(params.id);
+  if (isNaN(id)) return notFound();
+  const product = await getProduct(id);
+  if (!product) return notFound();
+  const isOwner = await getIsOwner(product.userId);
+  const onDelete = async () => {
+    "use server";
+    if (!isOwner) return;
+    await db.product.delete({
+      where: {
+        id,
+      },
+      select: null,
+    });
+    redirect("/products");
+  };
+  return (
+    <div>
+      <div className="relative aspect-square">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="px-3 py-5 flex items-center gap-3 border-b border-neutral-700">
+        <div className="size-10 rounded-full">
+          {product.user.avatar ? (
+            <Image
+              src={product.user.avatar}
+              alt={product.user.username}
+              width={40}
+              height={40}
+            />
+          ) : (
+            <UserIcon className="" />
+          )}
+        </div>
+        <div>
+          <h3>{product.user.username}</h3>
+        </div>
+      </div>
+      <div className="px-3 py-5 ">
+        <h1 className="text-2xl font-semibold">{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+      <div className="fixed w-full max-w-md bottom-0 mx-auto p-5 pb-10 bg-neutral-800 flex justify-between items-center">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
+        </span>
+        <div className="flex items-center gap-3">
+          <Link
+            className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+            href={``}
+          >
+            채팅하기
+          </Link>
+          {isOwner ? (
+            <form action={onDelete}>
+              <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+                Delete product
+              </button>
+            </form>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
