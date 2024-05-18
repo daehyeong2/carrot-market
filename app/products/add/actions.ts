a6cd9316ec7699758a97493560ed66fe0de4fe8a
daehyeong2 <baconbacon1231@gmail.com>
2024-05-19 00:54:39 +0900

    Form Action, Product Upload (11.1~11.2)

---
+++ app/products/add/actions.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { z } from "zod";
+import fs from "fs/promises";
+import { File } from "buffer";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "사진은 필수입니다.",
+  }),
+  title: z
+    .string({
+      required_error: "제목을 입력해 주세요.",
+    })
+    .min(3, "제목을 최소 3자 이상 입력해 주세요."),
+  description: z
+    .string({
+      required_error: "설명을 입력해 주세요.",
+    })
+    .min(3, "설명을 최소 3자 이상 입력해 주세요."),
+  price: z.coerce
+    .number({
+      required_error: "가격을 입력해 주세요.",
+    })
+    .min(1, "가격을 1원 이상 입력해 주세요."),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
+  const session = await getSession();
+  if (!session.id) return;
+  const data = {
+    photo: formData.get("photo"),
+    title: formData.get("title"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const product = await db.product.create({
+      data: {
+        title: result.data.title,
+        description: result.data.description,
+        price: result.data.price,
+        photo: result.data.photo,
+        user: {
+          connect: {
+            id: session.id,
+          },
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    redirect(`/products/${product.id}`);
+  }
+}
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,35 +3,78 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import React, { useState } from "react";
+import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 const AddProduct = () => {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const [state, action] = useFormState(uploadProduct, null);
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) return;
+    if (files.length !== 1) return alert("파일은 한개만 업로드해야 합니다.");
+    const MB = 1024 * 1024; // 1 MB
+    if (files[0].size > 5 * MB) return alert("파일은 최대 5MB입니다.");
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
   return (
     <div>
-      <form className="p-5 flex flex-col gap-3">
+      <form action={action} className="p-5 flex flex-col gap-3">
         <label
           htmlFor="photo"
-          className="border-2 aspect-square flex flex-col items-center justify-center gap-3 text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer"
+          className="border-2 aspect-square flex flex-col items-center justify-center gap-3 text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="size-20" />
-          <div className="text-sm text-neutral-400">사진을 추가해 주세요.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="size-20" />
+              <div className="text-sm text-neutral-400">
+                사진을 추가해 주세요.
+              </div>
+            </>
+          ) : null}
         </label>
+        {state?.fieldErrors.photo?.map((error, index) => (
+          <span key={index} className="text-red-500 font-medium">
+            {error}
+          </span>
+        ))}
         <input
           type="file"
           id="photo"
           name="photo"
           className="hidden"
+          accept="image/*"
           onChange={onImageChange}
+          required
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="number" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          required
+          placeholder="가격"
+          type="number"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           required
           placeholder="자세한 설명"
           type="text"
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
