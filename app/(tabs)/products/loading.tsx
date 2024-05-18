128049fd8d5870c448178045e304028b5243f0a3
daehyeong2 <baconbacon1231@gmail.com>
2024-05-18 14:15:18 +0900

    Skeletons (10.2)

---
+++ app/(tabs)/products/loading.tsx
@@ -0,0 +1,19 @@
+export default function Loading() {
+  return (
+    <>
+      <h1 className="text-white text-4xl">Products</h1>
+      <div className="p-5 animate-pulse flex flex-col gap-5">
+        {[...Array(3)].map((key) => (
+          <div className="*:rounded-md flex gap-5 items-center" key={key}>
+            <div className="size-28 bg-neutral-700" />
+            <div className="flex flex-col gap-2 *:bg-neutral-700 *:h-5 *:rounded-md">
+              <div className="w-40" />
+              <div className="w-20" />
+              <div className="w-30" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,9 @@
-const ProductsPage = () => {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+}
+
+const ProductsPage = async () => {
+  const products = await getProducts();
   return <h1 className="text-white text-4xl">Products</h1>;
 };
 
