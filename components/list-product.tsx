e083feaada2653cffa6c8e1f2d5c72cc7aa30fa1
daehyeong2 <baconbacon1231@gmail.com>
2024-05-18 19:33:27 +0900

    Recap (10.8)

--- components/list-product.tsx
+++ components/list-product.tsx
@@ -25,8 +25,8 @@ const ListProduct = ({
           <Image fill src={photo} alt={title} className="object-cover" />
         </div>
         <div className="flex flex-col gap-1 *:text-white">
-          <span className="text-2xl">{title}</span>
-          <span className="text-xl font-semibold">{formatToWon(price)}원</span>
+          <span className="text-xl">{title}</span>
+          <span className="text-lg font-semibold">{formatToWon(price)}원</span>
           <span className="text-sm text-neutral-500">
             {formatToTimeAgo(created_at + "")}
           </span>
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -12,10 +12,14 @@ interface ProductListProps {
 const ProductList = ({ initialProduct }: ProductListProps) => {
   const [products, setProducts] = useState(initialProduct);
   const [isLoading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setLastPage] = useState(false);
   const onLoadMore = async () => {
     setLoading(true);
-    const newProducts = await getMoreProducts(1);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length === 0) return setLastPage(true);
     setProducts((prev) => [...prev, ...newProducts]);
+    setPage((prev) => prev + 1);
     setLoading(false);
   };
   return (
@@ -23,13 +27,19 @@ const ProductList = ({ initialProduct }: ProductListProps) => {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        onClick={onLoadMore}
-        disabled={isLoading}
-        className="p-2 rounded-md text-sm font-semibold bg-orange-500 w-fit mx-auto disabled:bg-neutral-600 disabled:cursor-not-allowed active:scale-90"
-      >
-        {isLoading ? "더 불러오는 중.." : "더 불러오기"}
-      </button>
+      {!isLastPage ? (
+        <button
+          onClick={onLoadMore}
+          disabled={isLoading}
+          className="p-2 rounded-md text-sm font-semibold bg-orange-500 w-fit mx-auto disabled:bg-neutral-600 disabled:cursor-not-allowed active:scale-90"
+        >
+          {isLoading ? "더 불러오는 중.." : "더 불러오기"}
+        </button>
+      ) : (
+        <span className="text-neutral-600 mx-auto">
+          더 이상 상품이 없습니다.
+        </span>
+      )}
     </div>
   );
 };
