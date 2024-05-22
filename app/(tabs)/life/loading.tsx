2ff2b472e4eb8e694c56bde99ddc7ab27962d363
daehyeong2 <baconbacon1231@gmail.com>
2024-05-22 15:21:07 +0900

    See Posts, Likes and Dislikes (14.1~14.2)

---
+++ app/(tabs)/life/loading.tsx
@@ -0,0 +1,20 @@
+const Loading = () => {
+  return (
+    <div className="p-5 animate-pulse flex flex-col gap-5">
+      {[...Array(3)].map((_, idx) => (
+        <div className="*:rounded-md flex gap-5 items-center" key={idx}>
+          <div className="flex flex-col gap-2 *:rounded-md">
+            <div className="bg-neutral-700 h-5 w-20" />
+            <div className="bg-neutral-700 h-5 w-40" />
+            <div className="flex gap-2 *:rounded-md">
+              <div className="bg-neutral-700 h-5 w-5" />
+              <div className="bg-neutral-700 h-5 w-5" />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Loading;
